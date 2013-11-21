159c5349684641bd40519c6ebe20212a49b14efd
Nootan Ghimire <nootan.ghimire@gmail.com>
2013-11-20 21:13:12 -0800

    Added some water

--- system/scripts/main.js
+++ system/scripts/main.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	var stickyNavTop = $('.nav').offset().top;  
 
 	var stickyNav = function(){
-		var mainValue = $('#search-top-main').val();
+		/*var mainValue = $('#search-top-main').val();
 		var notMainValue = $('#search-top-notmain').val();
 		if((mainValue.length) >= (notMainValue.length)){
 			$('#search-top-notmain').val(mainValue);
 		} else {
 			$('#search-top-main').val(notMainValue);
-		}
+		}*/
 		var scrollTop = $(window).scrollTop();  
 
 		if (scrollTop > stickyNavTop) {   
@@ -23,4 +23,13 @@ $(document).ready(function() {
 	$(window).scroll(function() {  
 		stickyNav();  
 	});
+	
+	$('#search-top-main').change(function(){
+		$('#search-top-notmain').val($('#search-top-main').val());
+	});
+	
+	$('#search-top-notmain').change(function(){
+		$('#search-top-main').val($('#search-top-notmain').val());
+	})
+	
 });  
